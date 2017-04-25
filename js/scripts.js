100efec6855b61d0c89408ee29043f4231cd6cb0
Isabella Abatgis and Kristen McKee <isabella@>
2017-04-25 16:52:21 -0700

    fix this issues with an extra class that was not needed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
-// $(document).ready(function() {
-//   $(".panel-success").click(function() {
-//       $(".definition").slideToggle();
-//       $(".panel-title").slideToggle();
-//   });
-// });
-
 $(document).ready(function() {
   $(".panel-success").click(function() {
-    $(this).children().slideToggle("definition");
+    $(this).children().slideToggle();
   });
 });
